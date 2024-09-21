990685776b9d39793e97d75ead4ece32b5900261
Jeyasabari S <165381370+Jeyasabari05@users.noreply.github.com>
2024-09-21 20:40:11 +0530

    Time: 82 ms (100%), Space: 21.9 MB (14.29%) - LeetHub

---
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.c
@@ -0,0 +1,17 @@
+int* lexicalOrder(int n, int* returnSize) {
+    int* retNum = (int*)malloc(n * sizeof(int));
+    int v = 1;
+    for (int i = 0; i < n; i++) {
+        retNum[i] = v;
+        if (v * 10 <= n) {
+            v *= 10;
+        } else {
+            while (v % 10 == 9 || v == n) {
+                v /= 10;
+            }
+            v++;
+        }
+    }
+    *returnSize = n;
+    return retNum;
+}
